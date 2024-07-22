0dafa6a8ae155c29d35bb097a3863ce9d3d3feb1
Junaij Mohammed <junaij.mohammed@philips.com>
2024-07-22 15:16:34 +0530

    Initial verison of compute statistics

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,35 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+namespace Statistics
+{
+    Stats ComputeStatistics(const std::vector<double>& readings) 
+    {
+        Stats stats;
+        if(readings.empty())
+        {
+            stats.average = std::numeric_limits<double>::quiet_NaN();
+            stats.max = std::numeric_limits<double>::quiet_NaN();
+            stats.min = std::numeric_limits<double>::quiet_NaN();
+        }
+        else
+        {
+            stats.average = 0;
+            stats.max = readings[0];
+            stats.min = readings[0];
+            for(auto reading : readings)
+            {
+                stats.average += reading;
+                if(reading > stats.max)
+                {
+                    stats.max = reading;
+                }
+                if(reading < stats.min)
+                {
+                    stats.min = reading;
+                }
+            }
+            stats.average /= readings.size();
+        }
+        return stats;
+    }
 }
--- stats.h
+++ stats.h
@@ -1,7 +1,13 @@
 #include <vector>
 
-namespace Statistics {
+namespace Statistics 
+{
     // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
+    struct Stats {
+        double average;
+        double max;
+        double min;
+    };
+
+    Stats ComputeStatistics(const std::vector<double>& readings);
 }
